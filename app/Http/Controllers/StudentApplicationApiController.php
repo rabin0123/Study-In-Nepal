29621db910435f51340aa8ea0eecef3a44637738
Ajay Thakur <ajaythakurniit1@gmail.com>
2026-07-15 09:35:09 +0000

    A9683:20 PM1969PMve:{03}:{20}{PM}

--- app/Http/Controllers/StudentApplicationApiController.php
+++ app/Http/Controllers/StudentApplicationApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreStudentApplicationRequest;
 use App\Models\StudentApplication;
 use Barryvdh\DomPDF\Facade\Pdf;
