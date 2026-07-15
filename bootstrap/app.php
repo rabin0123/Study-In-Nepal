fc2b6ab4f42fd77e04ad52fceaad464e5ac01351
Rabin Pyakurel <rabinpyakurel9@gmail.com>
2026-07-15 03:09:18 +0000

    A2468:54 AM19618AMve: {08}:{54} {AM}

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         channels: __DIR__.'/../routes/channels.php',
         health: '/up',
-    ) 
+    )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
