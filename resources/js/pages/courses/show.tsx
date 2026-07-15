300517bcfc314c89ebee71ecd5607c9c2fc40cb2
Rabin Pyakurel <rabinpyakurel9@gmail.com>
2026-07-15 15:20:37 +0000

    A0059:05 PM19637PMve: {09}:{05} {PM}

--- resources/js/pages/courses/show.tsx
+++ resources/js/pages/courses/show.tsx
@@ -412,7 +412,7 @@ export default function CourseDetailsShow({ courseDetail }: Props) {
                 .gcu-hero-univ-name {
                     display: inline-block;
                     color: var(--color-white);
-                    font-size: 1.4rem;
+                    font-size: 1.1rem;
                     font-weight: 800;
                     margin-left: 100px;
                     text-shadow: 0 4px 15px rgba(0,0,0,0.4);
