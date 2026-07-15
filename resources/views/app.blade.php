5d8a76d680ff7af1d28287314f895016c4b70e98
Ajay Thakur <ajaythakurniit1@gmail.com>
2026-07-15 06:22:08 +0000

    A65712:07 PM1968PMve:{12}:{07}{PM}

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,6 +23,7 @@
                 }
             })();
         </script>
+        <script src="https://code.iconify.design/iconify-icon/3.1.1/iconify-icon.min.js"></script>
 
         {{-- Inline style to set the HTML background color based on our theme in app.css --}}
         <style>
