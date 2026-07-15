49be70055c74dee978b71731e1128f61cc6779ef
Ajay Thakur <ajaythakurniit1@gmail.com>
2026-07-15 08:08:37 +0000

    A7561:53 PM19637PMve:{01}:{53}{PM}

--- resources/js/pages/users/userindex.tsx
+++ resources/js/pages/users/userindex.tsx
@@ -546,7 +546,7 @@ export default function UsersIndex({
                                
                                 <thead className="text-dark fs-4" style={{ position: 'sticky', top: 0, zIndex: 1, background: 'var(--bs-card-bg, #fff)' }}>
                                     <tr>
-                                        <th className="ps-25">User Details</th>
+                                        <th className="ps-5">User Details</th>
                                         <th>Email</th>
                                         <th>Country</th>
                                         <th>Role</th>
