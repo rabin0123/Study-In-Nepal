e22d13edf683a5ec086ef236db63dfd44d0e0966
Rabin Pyakurel <rabinpyakurel9@gmail.com>
2026-07-16 02:25:34 +0000

    A5788:10 AM19734AMve: {08}:{10} {AM}

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::inertia('/', 'auth/login')->name('home');
 Route::post('/api/survey', [SurveyApiController::class, 'store']);
 Route::get('/course/{courseDetail:uuid}', [CourseDetailController::class, 'show'])->name('show');
-Route::get('/course-details/{courseDetail:uuid}', [CourseDetailController::class, 'show'])->name('show');
+Route::get('/course-details/{courseDetail:uuid}', [CourseDetailController::class, 'coursedetails'])->name('coursedetails');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
     Route::inertia('universities', 'university/universityentryform')->name('universityentryform');
