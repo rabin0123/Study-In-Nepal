2c2e97a78dcc0fb0051d8294d72f8f5f9950aa99
Rabin Pyakurel <rabinpyakurel9@gmail.com>
2026-07-16 03:38:57 +0000

    A4849:23 AM19757AMve: {09}:{23} {AM}

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,7 @@ Route::get('/courses/{university}', [CourseDetailController::class, 'show'])
     Route::get('/course-details/{courseDetail:uuid}/edit', [CourseDetailController::class, 'edit'])->name('edit');
     Route::put('/course-details/{courseDetail:uuid}', [CourseDetailController::class, 'update'])->name('update');
     Route::delete('/course-details/{courseDetail:uuid}', [CourseDetailController::class, 'destroy'])->name('destroy');
-    Route::get('/matched-courses', [CourseDetailController::class, 'getMatchedCourses'])->name('api.matched-courses');
+    Route::get('/course/resolve', [CourseDetailController::class, 'resolve'])->name('course.resolve');
 
 Route::prefix('api')->middleware(['auth', 'verified'])->group(function () {
 
