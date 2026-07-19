d79a67888ad7881f3811aa907deff407aa3ab80c
Ajay Thakur <ajaythakurniit1@gmail.com>
2026-07-19 09:36:30 +0000

    A7023:21 PM20030PMve:{03}:{21}{PM}

--- resources/js/pages/university/course/show.tsx
+++ resources/js/pages/university/course/show.tsx
@@ -12,6 +12,7 @@ type Props = {
         college_name: string;
         course_name: string;
         summary: string | null;
+        credit_hours: string | null;
         year_wise_modules: YearModule[] | null;
         fees: YearFee[] | null;
         careers: string | string[] | null;
