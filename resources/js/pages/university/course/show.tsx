f1b1a523420c213e6b1a0820b62a9945b1d6ddc8
Ajay Thakur <ajaythakurniit1@gmail.com>
2026-07-19 10:03:53 +0000

    A3223:48 PM20053PMve:{03}:{48}{PM}

--- resources/js/pages/university/course/show.tsx
+++ resources/js/pages/university/course/show.tsx
@@ -1104,7 +1104,7 @@ function ModuleAccordion({ name, info, credit_hours }: { name: string; info?: st
                 aria-expanded={hasInfo ? open : undefined}
             >
                 <span>{name}</span>
-                <span>{credit_hours}</span>
+                <span className="gcu-credit-hours">{credit_hours}</span>
                 {hasInfo && (
                     <span className="gcu-module-row__arrow" aria-hidden="true">
                         &#10142;
