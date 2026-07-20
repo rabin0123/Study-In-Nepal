607426227d7036137d96724feec70e95ebe6783b
Ajay Thakur <ajaythakurniit1@gmail.com>
2026-07-20 09:52:10 +0000

    A7243:37 PM20110PMve:{03}:{37}{PM}

--- app/Models/InstitutionalSurvey.php
+++ app/Models/InstitutionalSurvey.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class InstitutionalReadinessSurvey extends Model
+class InstitutionalSurvey extends Model
 {
     use HasFactory;
 
